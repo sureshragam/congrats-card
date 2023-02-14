875c6042534fd090acc76c0f561c5125b8f45253
suresh <sureshyadavragam@gmail.com>
2023-02-14 07:38:24 +0000

    added content to index.js and index.css file

---
+++ index.js
@@ -0,0 +1,14 @@
+const element = (
+  // Write your code here.
+  <div>
+    <h1 className="heading">Congratulations</h1>
+    <div className="card">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h1 className="profile-name">Kiran V</h1>
+      <p>Vishu Institue of Computer and Technology Bhimavaram</p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
